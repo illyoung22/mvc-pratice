5f04d084028d54c426b46741cec33920115cc4b6
illyoung22 <illyoung22@hanmail.net>
2023-09-19 15:54:22 +0900

    패스트캠프-테스트 코드d

--- src/main/java/org/example/CorrectFixedPasswordGenerator.java
+++ src/main/java/org/example/CorrectFixedPasswordGenerator.java
@@ -3,6 +3,6 @@ package org.example;
 public class CorrectFixedPasswordGenerator implements PasswordGenerator{
     @Override
     public String generatePassword() {
-        return "abcdefgh"; //8글자
+        return "abcdefgh"; //8글자cd
     }
 }
