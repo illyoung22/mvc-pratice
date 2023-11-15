c63db59c5dd4561f88c4f0259c6728e44fa970ee
illyoung22 <illyoung22@hanmail.net>
2023-11-16 07:41:16 +0900

    mvcste

--- src/test/java/org/example/ReflectionTest.java
+++ src/test/java/org/example/ReflectionTest.java
@@ -20,7 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Controller 애노테이션이 설정되어 있는 모든 클래스를 찾아서 출력
- * [ReFlection]s
+ * [ReFlection]sr
  * - 힙 영역에 로드되어 있는 클래스 타입의 객체를 통해 필드/메소드/생성자를 접근 제어자와 상관 없이 사용할 수 있도록 지원하는 API
  * - 컴파일 시점이 아닌 런타임 시점에 동적으로 특정 클래스의 정보를 추출할 수 있는 프로그래밍 기법
  * - 주로 프레임 워크 또는 라이브러리 개발 시 사용
